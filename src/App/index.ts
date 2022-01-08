cbbd4375a0190e9283c78c2294ef6d08e642c52a
你好2007 <2501482523@qq.com>
2022-01-09 02:13:53 +0800

    添加缩放监听

--- src/App/index.ts
+++ src/App/index.ts
@@ -5,6 +5,8 @@ import image3D from 'image3d'
 import vertexShader from './shader-vertex.c'
 import fragmentShader from './shader-fragment.c'
 
+import doResize from '../tool/ResizeObserver'
+
 import style from './index.scss'
 import template from './index.html'
 
@@ -15,34 +17,46 @@ import template from './index.html'
 })
 export default class {
 
-    size: number
+    width: number
+    height: number
 
     $setup() {
         return {
-            size: ref(window.innerWidth > window.innerHeight - 30 ? window.innerHeight - 30 : window.innerWidth)
+            width: ref(0),
+            height: ref(0)
         }
     }
 
     $mounted() {
-        setTimeout(() => {
+        let el = document.getElementById('canvas')
 
-            // 创建3D对象并配置好画布和着色器
-            let image3d = new image3D(document.getElementsByTagName('canvas')[0], {
-                "vertex-shader": vertexShader,
-                "fragment-shader": fragmentShader,
-                depth: true
+        doResize(el, () => {
+            this.width = el.clientWidth
+            this.height = el.clientHeight
+
+            setTimeout(() => {
+                this.doit()
             })
-
-            let painter = image3d.Painter()
-            // let buffer = image3d.Buffer()
-            // let camera = image3d.Camera()
-
-            image3d.setAttributeFloat("a_position", 0, 0, 0, 1)
-            image3d.setUniformFloat("u_color", 0.8, 0.8, 0.8, 1)
-
-            painter.drawPoint(0, 1);
-
         })
+    }
+
+    doit() {
+
+        // 创建3D对象并配置好画布和着色器
+        let image3d = new image3D(document.getElementsByTagName('canvas')[0], {
+            "vertex-shader": vertexShader,
+            "fragment-shader": fragmentShader,
+            depth: true
+        })
+
+        let painter = image3d.Painter()
+        let buffer = image3d.Buffer()
+        let camera = image3d.Camera()
+
+        image3d.setAttributeFloat("a_position", 0, 0, 0)
+        image3d.setUniformFloat("u_color", 0.8, 0.8, 0.8, 1)
+
+        painter.drawPoint(0, 1);
     }
 
 }
